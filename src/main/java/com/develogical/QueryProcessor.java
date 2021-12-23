5a93a972fd7afd0b369e3fcc3ae8c440c94c4a29
adammcg97 <adammorley8@aol.com>
2021-12-23 14:58:21 +0000

    Wrap query processing

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,10 +1,19 @@
 package com.develogical;
 
-import java.util.Arrays;
 
 public class QueryProcessor {
 
     public String process(String query) {
+        try {
+            return processQuery(query);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return "Error processing query" ;
+        }
+
+    }
+
+    private String processQuery(String query) {
         String actualQuery = query.substring(query.indexOf(":") + 1);
         if (actualQuery.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
@@ -20,7 +29,6 @@ public class QueryProcessor {
         else {
             return "";
         }
-
     }
 
     private String[] getNumbersFromString(String query) {
